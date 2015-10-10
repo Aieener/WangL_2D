9b53a7e55e7f926f13e25b313c5b24a1ce9b06ae
Aieener <aikurt@hotmail.com>
2015-10-10 19:22:27 -0400

    fix main.cpp to record time

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main()
 {
+	stringstream t;
 	double start = clock();
 
 	// ======================= MCRUN & Plotting the final config ===============================
@@ -16,6 +17,11 @@ int main()
 	// m.MCWEIGHT();
 	// ======================= end of simulation, print out the time =======
 	double end = clock();
-	cout <<"This simulation takes "<< (double(end-start)/CLOCKS_PER_SEC)<<endl;
+	cout <<"This simulation takes "<< (double(end-start)/CLOCKS_PER_SEC)<<" sec."<<endl;
+	t <<"This simulation takes "<< (double(end-start)/CLOCKS_PER_SEC) <<" sec."<<endl;
+	ofstream myfile ("time.txt");
+	string datat = t.str();
+	myfile << datat;
+	myfile.close();	
 	return 0;
 }
